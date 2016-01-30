5e7a6c8e5a9a78ddf53e235efccf95284c1d1ec4
Varun Goel <varun.goel@stonybrook.edu>
2016-01-30 13:35:17 -0500

    Updated to load lower resolution image
    Added functionality: First lower resolution image is loaded and then the higher resolution image is loaded. Useful for slower internet connections.

--- flicks files/DeailViewController.swift
+++ flicks files/DeailViewController.swift
@@ -20,36 +20,90 @@
     @IBOutlet weak var scrollView: UIScrollView!
     
     @IBOutlet weak var containerView: UIView!
-        var movie:NSDictionary!
+    var movie:NSDictionary!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         scrollView.contentSize = CGSize(width: scrollView.frame.size.width, height: containerView.frame.origin.y + containerView.frame.size.height-58)
         
-        
-        
-        //containerView.frame.size.height = 290
-        
-        //base url of an image extracted from the API
-        let baseURL = "http://image.tmdb.org/t/p/w500"
-        
         titleLabel.text = movie["title"] as? String
         
         overviewLabel.text = movie["overview"] as? String
         overviewLabel.sizeToFit()
         
+        //base url of the lower resolution imahe
+        let smallURL = "https://image.tmdb.org/t/p/w45"
+        
+        //default url
+        let defaultURL = "https://image.tmdb.org/t/p/w500"
+        
+        //base url of an image extracted from the API for the high res image
+        let largeURL = "http://image.tmdb.org/t/p/original"
+        
+        //the total url for the lower resolution image
+        //let smallImageURL = NSURL(string:smallURL + posterPath)
+        
+        //the total url for the higher resolution image
+        //let smallImageURL = NSURL(string:largeURL + posterPath)
+        
+        
+        
         
         //safety check in case the particular movie doesn't have a poster
         if let posterPath = movie["poster_path"] as? String {
-            let imageURL = NSURL(string:baseURL + posterPath)
-            posterImageView.setImageWithURL(imageURL!)
+            
+            //the total url for the lower resolution image
+            let smallImageURL = NSURL(string:smallURL + posterPath)
+            
+            //the total url for the default resolution image
+            let defaultImageURL = NSURL(string:defaultURL + posterPath)
+            
+            //the total url for the higher resolution image
+            let largeImageURL = NSURL(string:largeURL + posterPath)
+            
+            let smallImageRequest = NSURLRequest(URL: smallImageURL!)
+
+            let largeImageRequest = NSURLRequest(URL: largeImageURL!)
+            
+            posterImageView.setImageWithURLRequest(
+                smallImageRequest,
+                placeholderImage: nil,
+                success: { (smallImageRequest, smallImageResponse, smallImage) -> Void in
+                    
+                    // smallImageResponse will be nil if the smallImage is already available
+                    // in cache (might want to do something smarter in that case).
+                    self.posterImageView.alpha = 0.0
+                    self.posterImageView.image = smallImage;
+                    
+                    UIView.animateWithDuration(0.3, animations: { () -> Void in
+                        
+                        self.posterImageView.alpha = 1.0
+                        
+                        }, completion: { (sucess) -> Void in
+                            
+                            // The AFNetworking ImageView Category only allows one request to be sent at a time
+                            // per ImageView. This code must be in the completion block.
+                            self.posterImageView.setImageWithURLRequest(
+                                largeImageRequest,
+                                placeholderImage: smallImage,
+                                success: { (largeImageRequest, largeImageResponse, largeImage) -> Void in
+                                    
+                                    self.posterImageView.image = largeImage;
+                                    
+                                },
+                                failure: { (request, response, error) -> Void in
+                                    self.posterImageView.setImageWithURL(defaultImageURL!)
+                            })
+                    })
+                },
+                failure: { (request, response, error) -> Void in
+                    self.posterImageView.setImageWithURL(defaultImageURL!)
+            })
         }
         
         
         
-        // Do any additional setup after loading the view.
-        // print(movie)
     }
     
     override func didReceiveMemoryWarning() {
